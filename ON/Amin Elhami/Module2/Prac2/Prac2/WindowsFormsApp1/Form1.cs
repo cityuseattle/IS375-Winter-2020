f9bbbac027182a057af1d7965e5bbae192ba869e
Amin <amin747@gmail.com>
2020-01-19 20:54:40 -0800

    submission for mod2

---
+++ ON/Amin Elhami/Module2/Prac2/Prac2/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Windows0
+{
+    public class Form1: Form
+    {
+        public Form1()
+        {
+            Text = "Simple Application";
+        }
+        
+        static void Main()
+        {
+            Form1 winForm = new Form1();
+            Application.Run(winForm);
+        }
+    }
+}
