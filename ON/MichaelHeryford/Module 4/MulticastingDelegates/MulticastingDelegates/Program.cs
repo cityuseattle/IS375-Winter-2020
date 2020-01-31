1fb569bc2261785782fc2edc145956130c08c5b6
Student <student123432@gmail.com>
2020-01-31 08:12:05 +0500

    Module 4 submission

---
+++ ON/MichaelHeryford/Module 4/MulticastingDelegates/MulticastingDelegates/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MulticastingDelegates
+{
+    class Rectangle
+    {
+        //declraing delegates
+        public delegate void rectDelegate(double height, double width);
+        //area method
+        public void area(double height, double width)
+        {
+            Console.WriteLine("Area is: {0}", (width * height));
+
+        }
+        //perimeter method
+        public void perimeter(double height, double width)
+        {
+            Console.WriteLine("Perimeter is: {0}", 2 * (width + height));
+        }
+        //main method
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+
+            rectDelegate rectDelegate = new rectDelegate(rectangle.area);
+
+            rectDelegate += rectangle.perimeter;
+
+            rectDelegate.Invoke(6.3, 4.2);
+            Console.WriteLine();
+
+            rectDelegate.Invoke(16.3, 10.3);
+        }
+    }
+}
---
+++ ON/MichaelHeryford/Module 4/PredicateDelegates/PredicateDelegates/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace PredicateDelegates
+{
+    class Program
+    {
+        public static bool myfun(string myString)
+        {
+            if (myString.Length < 7)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Predicate<string> val = myfun;
+            Console.WriteLine(val("GeeksforGeeks"));
+        }
+    }
+}
