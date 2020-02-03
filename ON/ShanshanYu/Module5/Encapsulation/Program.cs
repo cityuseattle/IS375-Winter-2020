02033aa0afa2d9a673e99bdd2b193654c1ce9119
Shanshan Yu <yushanshan@cityuniversity.edu>
2020-02-02 23:59:29 -0800

    Module 5

---
+++ ON/ShanshanYu/Module5/Encapsulation/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Encapsulation
+{
+   public class DemoEncap
+   {
+      private String studentName;
+      private int studentAge;
+      public string Name
+      {
+         get { return studentName; }
+         set { studentName = value; }
+      }
+      public int Age
+      {
+         get { return studentAge; }
+         set { studentAge = value; }
+      }
+   }
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         DemoEncap obj = new DemoEncap();
+         obj.Name = "ShanshanYu";
+         obj.Age = 18;//girls are always 18!!!!
+
+         Console.WriteLine("Name: " + obj.Name);
+         Console.WriteLine("Age: " + obj.Age);
+      }
+   }
+}
