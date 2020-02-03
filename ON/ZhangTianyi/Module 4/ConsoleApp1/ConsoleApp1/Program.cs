aeb2275b6d7499846fc8575ae60bf091cd685e62
Tianyi Zhang <zhangtianyi@cityuniversity.edu>
2020-02-02 21:07:36 -0800

    submission for section2 module 4

---
+++ ON/ZhangTianyi/Module 4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public static void myfun(int p, int q)
+        {
+            Console.WriteLine(p - q);
+        }
+        static public void Main()
+        {
+            Action<int, int> val = myfun;
+            val(20, 5);
+        }
+    }
+
+}
