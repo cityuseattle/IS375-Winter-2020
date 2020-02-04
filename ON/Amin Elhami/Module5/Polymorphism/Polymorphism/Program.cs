3d2b1e6507a22b19861aeb83413135157532f1ef
Amin <amin747@gmail.com>
2020-02-03 18:11:20 -0800

    Submission for Module 5

---
+++ ON/Amin Elhami/Module5/Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Polymorphism
+{
+    class Printdata
+    {
+        void print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", i);
+        }
+        void print(double f)
+        {
+            Console.WriteLine("Printing float: {0}", f);
+        }
+        void print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+        static void Main(string[] args)
+        {
+            Printdata p = new Printdata();
+            p.print(5);
+            p.print(500.263);
+            p.print("Hello C++");
+            Console.ReadKey();
+        }
+    }
+}
