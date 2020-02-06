0361808b19da6ce80f35048058e010c0a2ad0988
Student <student123432@gmail.com>
2020-02-06 09:57:01 +0500

    Module 5 submission

---
+++ ON/MichaelHeryford/Module 5/Inheritance/Inheritance/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Shape
+    {
+        public void setWidth(int w)
+        {
+            width = w;
+        }
+        public void setHeight(int h)
+        {
+            height = h;
+        }
+
+        protected int width;
+        protected int height;
+    }
+    class Rectangle : Shape
+    {
+        public int getArea()
+        {
+            return (width * height);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rectangle = new Rectangle();
+            rectangle.setWidth(5);
+            rectangle.setHeight(7);
+
+            Console.WriteLine("Total area: {0}", rectangle.getArea());
+        }
+    }
+}
---
+++ ON/MichaelHeryford/Module 5/StaticPolymprphism/StaticPolymprphism/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StaticPolymprphism
+{
+    class PrintData
+    {
+        void print(int i)
+        {
+            Console.WriteLine("Printing int: {0}", i);
+        }
+        void print(double f)
+        {
+            Console.WriteLine("Printing double: {0}", f);
+        }
+        void print(string s)
+        {
+            Console.WriteLine("Printing string: {0}", s);
+        }
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                PrintData printData = new PrintData();
+
+                printData.print(5);
+                printData.print(500.263);
+                printData.print("Hello C#");
+                Console.ReadKey();
+            }
+        }
+    }
+}
