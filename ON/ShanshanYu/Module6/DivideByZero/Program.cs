69efe4adcada05ec840900b006c1f4327feec416
Shanshan Yu <yushanshan@cityuniversity.edu>
2020-02-08 14:59:03 -0800

    Module6

---
+++ ON/ShanshanYu/Module6/DivideByZero/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DivideByZero
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         int[] number = {8,17,24,5,25 };
+         int[] divisor = { 2, 0, 0, 5 };
+
+         for (int j = 0; j < number.Length; j++)
+         {
+            try
+            {
+               Console.WriteLine("Number: {0}", number[j]);
+               Console.WriteLine("Divisor: {0}", divisor[j]);
+               Console.WriteLine("Quotient: {0}", number[j] / divisor[j]);
+            }
+            catch (DivideByZeroException)
+            { Console.WriteLine("Not possible to Divide is zero."); }
+            catch (IndexOutOfRangeException)
+            { Console.WriteLine("Indet is Out of rage."); }
+         }
+      }
+   }
+}
---
+++ ON/ShanshanYu/Module6/UserDefinedException/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace UserDefinedException
+{
+   class Program
+   {
+      static void Main(string[] args)
+      {
+         Temprature temp = new Temprature();
+         try
+         {
+            temp.ShowTemp();
+         }
+         catch (TempIsZeroException e)
+         {
+            Console.WriteLine("TempIsZeroException: {0}", e.Message);
+         }
+      }
+   }
+
+   public class TempIsZeroException : Exception
+   {
+      public TempIsZeroException(string message) : base(message)
+      {
+
+      }
+   }
+   public class Temprature
+   {
+      int temprature = 0;
+      public void ShowTemp()
+      {
+         if (temprature == 0)
+         { throw (new TempIsZeroException("Zero Temprature found")); }
+         else
+         { Console.WriteLine("Temrature: {0}", temprature); }
+      }
+   }
+
+   
+}
