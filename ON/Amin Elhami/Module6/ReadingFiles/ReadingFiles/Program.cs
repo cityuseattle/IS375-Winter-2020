fdc3f2a1d4a9de498918267c784d7a3ff6b3a6ea
Amin <amin747@gmail.com>
2020-02-11 16:31:09 -0800

    Submission Module 6

---
+++ ON/Amin Elhami/Module6/ReadingFiles/ReadingFiles/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                using (StreamReader sr = new StreamReader("C:/Users/Amin/Desktop/Courses/IS375-Winter-2020/ON/Amin Elhami/Module6/jamaica.txt"))
+                {
+                    string line;
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("The file could not be read");
+                Console.WriteLine(e.Message);
+            }
+
+        }
+    }
+
+}
