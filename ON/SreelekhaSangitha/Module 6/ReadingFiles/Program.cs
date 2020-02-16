721841e12c81c556ae07256523d85b6025902aaa
Sreelekha Sangitha <sangithasreelekha@cityuniversity.edu>
2020-02-15 22:19:57 -0800

    Submission for Module 6

---
+++ ON/SreelekhaSangitha/Module 6/ReadingFiles/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace ReadingFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                //Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+
+                using (StreamReader sr = new StreamReader("C:/Users/sangi/source/repos/sreelekhasangitha/IS375-Winter-2020/ON/SreelekhaSangitha/Module 6/Sangitha.txt"))
+                   
+                {
+                    string line;
+
+                    // Read  and display lines from the file until
+                    // the end of the file is reached.
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                // Let the user know what went wrong
+                Console.WriteLine("The file could not be read");
+                Console.WriteLine(e.Message);
+            }
+        
+        }
+    }
+}
