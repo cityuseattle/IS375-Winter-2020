150c8356c3d3cca8bf6f6f2d4ad7fc9ea6e324af
Gabby W <40610520+gabbywargin@users.noreply.github.com>
2020-02-15 21:42:08 -0800

    Submission for Module6

---
+++ ON/Module6/ReadingFiles/ReadingFiles/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.IO;
+
+
+namespace ReadingFiles
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            try
+            {
+                // Create an instance of StreamReader to read from a file.
+                // The using statement also closes the StreamReader.
+                using (StreamReader sr = new StreamReader(@"C:\Users\gabby\OneDrive\Desktop\C++ Intermediate\IS375-Winter-2020\ON\GabrielaWargin\Module6\ReadingFiles\jamaica.txt"))
+                {
+                    string line;
+
+                    // Read and display lines from file until
+                    // the end of the file is reached.
+
+                    while ((line = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(line);
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                //Let the user know what went wrong.
+                Console.WriteLine("The file could not be read: ");
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
