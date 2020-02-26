96253e97f432a389b3d82de4bdedd41710cf9421
Amin <amin747@gmail.com>
2020-02-25 16:09:49 -0800

    Submission Module 8

--- ON/Amin Elhami/Module7/DBConnection/DBConnection/Form1.cs
+++ ON/Amin Elhami/Module7/DBConnection/DBConnection/Form1.cs
@@ -22,10 +22,97 @@ namespace DBConnection
         {
             string connectionString;
             SqlConnection myconnection;
-            connectionString = "Server=localhost;Database=master;Trusted_Connection=True;";
+            connectionString = "Server=localhost;Database=Amin2;Trusted_Connection=True;";
             myconnection = new SqlConnection(connectionString);
             myconnection.Open();
-            MessageBox.Show("Connection Open Successfully");
+
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            sql = "Select ID, Name from Student";
+            command = new SqlCommand(sql, myconnection);
+            dataReader = command.ExecuteReader();
+
+            while (dataReader.Read())
+            {
+                Output += dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + "\n";
+            }
+
+            MessageBox.Show(Output);
+            dataReader.Close();
+            command.Dispose();
+            myconnection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection myconnection;
+            connectionString = "Server=localhost;Database=Amin2;Trusted_Connection=True;";
+            myconnection = new SqlConnection(connectionString);
+            myconnection.Open();
+
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql = "";
+
+            sql = "Insert into Student (ID, Name) values (4, 'Jack')";
+            MessageBox.Show("Added Successfully!");
+
+            command = new SqlCommand(sql, myconnection);
+
+            adapter.InsertCommand = new SqlCommand(sql, myconnection);
+            adapter.InsertCommand.ExecuteNonQuery();
+            command.Dispose();
+            myconnection.Close();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection myconnection;
+            connectionString = "Server=localhost;Database=Amin2;Trusted_Connection=True;";
+            myconnection = new SqlConnection(connectionString);
+            myconnection.Open();
+
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql = "";
+
+            sql = "Update Student set Name='Jones' where ID=4";
+            MessageBox.Show("Added Successfully!");
+
+            command = new SqlCommand(sql, myconnection);
+
+            adapter.UpdateCommand = new SqlCommand(sql, myconnection);
+            adapter.UpdateCommand.ExecuteNonQuery();
+
+            command.Dispose();
+            myconnection.Close();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection myconnection;
+            connectionString = "Server=localhost;Database=Amin2;Trusted_Connection=True;";
+            myconnection = new SqlConnection(connectionString);
+            myconnection.Open();
+
+            SqlCommand command;
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql = "";
+
+            sql = "Delete Student where Name='Jones'";
+            MessageBox.Show("Deleted Successfully!");
+
+            command = new SqlCommand(sql, myconnection);
+
+            adapter.DeleteCommand = new SqlCommand(sql, myconnection);
+            adapter.DeleteCommand.ExecuteNonQuery();
+
+            command.Dispose();
             myconnection.Close();
         }
     }
