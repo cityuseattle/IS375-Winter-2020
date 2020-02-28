b5fb9c9e76d84000be1b5d979bf56093cd578e8b
Dyan Cubacub <dcubacub@cityuniversity.edu>
2020-02-27 21:46:42 -0800

    Submission for Module 8

--- ON/DyanCubacub/Module7/DBConnection/DBConnection/Form1.cs
+++ ON/DyanCubacub/Module7/DBConnection/DBConnection/Form1.cs
@@ -22,10 +22,111 @@ namespace DBConnection
         {
             string connectionString;
             SqlConnection myconnection;
-            connectionString = "Server=localhost;Database=master;Trusted_Connection=True;";
+            connectionString = "Server=localhost;Database=myDB;Trusted_Connection=True;";
             myconnection = new SqlConnection(connectionString);
             myconnection.Open();
-            MessageBox.Show("Connection Open Successfully!");
+            // MessageBox.Show("Connection Open Successfully!");
+            
+            SqlCommand command;
+            SqlDataReader dataReader;
+            String sql, Output = "";
+
+            // define SQL statement
+            sql = "Select ID, Name from Student";
+            // the command statment used to perform operations of reading and writing into the database
+            command = new SqlCommand(sql, myconnection);
+            // define data reader used to get all the data specified by the SQL query 
+            dataReader = command.ExecuteReader();
+
+            // det the table values
+            while (dataReader.Read())
+            {
+                Output += dataReader.GetValue(0) + " - " + dataReader.GetValue(1) + "\n";
+            }
+
+            MessageBox.Show(Output);
+
+            // good practice to close all the objects related to out database operation
+            dataReader.Close();
+            command.Dispose();
+            myconnection.Close();
+        }
+
+        private void Insert_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection myconnection;
+            connectionString = "Server=localhost;Database=myDB;Trusted_Connection=True;";
+            myconnection = new SqlConnection(connectionString);
+            myconnection.Open();
+            
+            SqlCommand command;
+
+            // SqlDataAdapter used to perform specific SQL operations such as insert, delete, and update commands.
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql = "";
+
+            sql = "Insert into Student (ID,Name) values(4, 'Jack' )";
+            MessageBox.Show("Added Successfully!");
+
+            command = new SqlCommand(sql, myconnection);
+
+            adapter.InsertCommand = new SqlCommand(sql, myconnection);
+            adapter.InsertCommand.ExecuteNonQuery();
+
+            command.Dispose();
+            myconnection.Close();
+        }
+
+        private void Update_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection myconnection;
+            connectionString = "Server=localhost;Database=myDB;Trusted_Connection=True;";
+            myconnection = new SqlConnection(connectionString);
+            myconnection.Open();
+
+            SqlCommand command;
+
+            // SqlDataAdapter used to perform specific SQL operations such as insert, delete, and update commands.
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql = "";
+
+            sql = "Update Student set Name= 'Jones' where ID=4";
+            MessageBox.Show("Updated Successfully!");
+
+            command = new SqlCommand(sql, myconnection);
+
+            adapter.UpdateCommand = new SqlCommand(sql, myconnection);
+            adapter.UpdateCommand.ExecuteNonQuery();
+
+            command.Dispose();
+            myconnection.Close();
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            string connectionString;
+            SqlConnection myconnection;
+            connectionString = "Server=localhost;Database=myDB;Trusted_Connection=True;";
+            myconnection = new SqlConnection(connectionString);
+            myconnection.Open();
+
+            SqlCommand command;
+
+            // SqlDataAdapter used to perform specific SQL operations such as insert, delete, and update commands.
+            SqlDataAdapter adapter = new SqlDataAdapter();
+            String sql = "";
+
+            sql = "Delete Student where Name='Jones'";
+            MessageBox.Show("Deleted Successfully!");
+
+            command = new SqlCommand(sql, myconnection);
+
+            adapter.DeleteCommand = new SqlCommand(sql, myconnection);
+            adapter.DeleteCommand.ExecuteNonQuery();
+
+            command.Dispose();
             myconnection.Close();
         }
     }
