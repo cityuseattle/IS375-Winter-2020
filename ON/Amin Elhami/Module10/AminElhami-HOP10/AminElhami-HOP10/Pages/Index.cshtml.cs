50eb8e3aeefefa3784f89ee5f51e093900fd7ddc
Amin <amin747@gmail.com>
2020-03-10 17:51:37 -0700

    Submission for Module 10

---
+++ ON/Amin Elhami/Module10/AminElhami-HOP10/AminElhami-HOP10/Pages/Index.cshtml.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using AminElhami_HOP10.Models;
+
+namespace AminElhami_HOP10.Pages
+{
+    public class IndexModel : PageModel
+    {
+        [BindProperty]
+        public Account account { get; set; }
+        public void OnGet()
+        {
+            account = new Account();
+        }
+
+        public IActionResult OnPost(Account account)
+        {
+            if (account.Username != null && account.Username.Equals("abc"))
+            {
+                ModelState.AddModelError("account.Username", "Username Already Exists");
+            }
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            else
+            {
+                return RedirectToPage("Success");
+            }
+        }
+    }
+}
